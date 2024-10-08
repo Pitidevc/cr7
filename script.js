7b09f8050be497fcd4f9fcb6202b28d556d7feb9
pitidevc <angelsteven1245@gmail.com>
2024-10-08 11:15:37 -0500

    el bicho

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("messageButton").addEventListener("click", function() {
+    const messageElement = document.getElementById("message");
+    messageElement.textContent = "¡Cristiano Ronaldo es una leyenda del fútbol!";
+    messageElement.style.display = "block";
+});
